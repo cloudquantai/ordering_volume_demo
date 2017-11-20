81ec80668d784c48742f0503cbbad1f4b8fe91d5
CloudQuant <33037983+cloudquantai@users.noreply.github.com>
2017-11-20 09:25:56 -0600

    Update share_order_demo.py

--- share_order_demo.py
+++ share_order_demo.py
@@ -7,10 +7,6 @@ from cloudquant.interfaces import Strategy
 from cloudquant.util import dt_from_muts
 import numpy as np
 
-# These functions are for the midpoint peg, they will only work in the ELITE Cloudquant Version. Delete or Comment out for Lite.
-lime_midpoint_limit_buy = "4e69745f-5410-446c-9f46-95ec77050aa5"
-lime_midpoint_limit_sell = "23d56e4a-ca4e-47d0-bf60-7d07da2038b7"
-
 end_delay = 20  # in minutes, how long before the end of the day to stop trading.
 start_delay = 10  # in minutes, how long after market open before we start trading
 
@@ -72,41 +68,26 @@ class breakout_purchase(Strategy):
 
                         # go long if stock is above its normal values
                         if (average_high) < bar_close[0]:
-                            # Now that we know we are going long, we have to figure out the best way to do it. First, we determine
-                            # the number of shares
                             
-                            num_shares = np.round(purchase_amount / md[self.symbol].L1.last)
+                            #Now that we know we are going long, we have to figure out how many shares to purchase
+                            
+                            #One option is to just use a fixed number of shares
+                            num_shares=100
+                            
+                            #Another is to allocate the amount you purchase based on the volume at the last minute.
+                            #You have options that include md[self.symbol].L1.ask_size and md[self.symbol].L1.minute_volume
+                            #For this example I use the bar's askvol, which I have found to be the most accurate.
+                            num_shares=int(bar_askvol[0]*.1)
+                            
+                            #Finally, we have the option of purchasing a fixed dollar amount of a particular share.
+                            #This is the recommended way. You can adjust the amount based on how much risk you want to take.
+                            num_shares=np.round(purchase_amount/md[self.symbol].L1.last)
 
-                            # To execute the trade, we have a few options. First, we could just purchase at market value,
-                            # which means we buy at the price someone says they are willing to pay.
-                            # You probably wouldn't do this live trading but it works for backtesting.
+                            # Play around with the different volumes and see how they affect the performance.
+                            
+                            
 
-                            # order_id = order.algo_buy(self.symbol, algorithm="market", intent="init", order_quantity=num_shares)
-
-                            # Alternatively, we can use a limit order. Exactly what limit you choose depends on how aggressively
-                            # you want to place your position. Placing a limit at the ask is probably going to be very similar
-                            # to a market order in backtesting, but will avoid a pull to higher values if the stock swings up.
-                            # you can go higher than the market price as well, if you want to be extra sure your order will fill
-                            # in a price range that is controlled. If you don't need your order filled immediately, you can pick
-                            # a limit value that is between the bid and ask price. You'll get a better price for your shares, but
-                            # your order may not fill.
-                            # Currently in the CQ environment, my limit orders seem to fill pretty readily below ask price, which
-                            # would not reflect real trading. So be careful to pick realistic numbers for your limits.
-
-                            # order_id = order.algo_buy(self.symbol, algorithm="limit", price=md[self.symbol].L1.ask-.01, intent="init", order_quantity=num_shares)
-                            # order_id = order.algo_buy(self.symbol, algorithm="limit", price=md[self.symbol].L1.ask*.99, intent="init", order_quantity=num_shares)
-
-                            # Finally, we can do what is called a midpoint-peg. Check the references for a mathematical description, but
-                            # it's essentially the midpoint between bid and ask prices. It is only available in ELITE. You can also set
-                            # the price as well, though I'm not entirely sure how the algorithm incorporates that price into its what it
-                            # predicts. It works very well, but if your algorithm requires getting your stocks quickly, it may not be the
-                            # best choice. It will make your algorithm look better than it is because it has unrealistic price points for
-                            # your purchases.
-
-                            # order_id = order.algo_buy(self.symbol, algorithm=lime_midpoint_limit_buy, price=md[self.symbol].L1.ask*1.05, intent="init", order_quantity=num_shares)
-                            order_id = order.algo_buy(self.symbol, algorithm=lime_midpoint_limit_buy,
-                                                      price=md[self.symbol].L1.ask + .05, intent="init",
-                                                      order_quantity=num_shares)
+                            order_id = order.algo_buy(self.symbol, algorithm="market", intent="init", order_quantity=num_shares)
 
                             print('Purchasing {0} after breakout at {1}, purchased {2} shares at {3}' \
                                   .format(self.symbol, service.time_to_string(service.system_time), num_shares,
@@ -122,19 +103,7 @@ class breakout_purchase(Strategy):
                         # the stock has dropped too low, exit out of this position
                         if self.entry_price * stop_loss_ratio > bar_close[0]:
 
-                            # We have the same options for selling that we had for purchasing. We can do a market order.
-
-                            # sell_order_id = order.algo_sell(self.symbol, algorithm="market", intent="exit")
-
-                            # Or a limit order, some value around the ask and bid prices.
-
-                            # sell_order_id = order.algo_sell(self.symbol, algorithm="limit", price=md[self.symbol].L1.bid-.05, intent="exit")
-                            # sell_order_id = order.algo_sell(self.symbol, algorithm="limit", price=md[self.symbol].L1.bid*.95, intent="exit")
-
-                            # Finally, a midpoint peg, again, this only works in CloudQuant Elite.
-
-                            sell_order_id = order.algo_sell(self.symbol, algorithm=lime_midpoint_limit_sell,
-                                                            intent="exit")
+                            sell_order_id = order.algo_sell(self.symbol, algorithm="market", intent="exit")
 
                             print('selling out of {0} at {1} due to stock dropping below average high at {1}'.format(
                                 self.symbol, service.time_to_string(service.system_time), bar_close[0]))
@@ -144,19 +113,7 @@ class breakout_purchase(Strategy):
                         # we've made our target profit, let's back out of the trade now
                         elif self.entry_price * stop_profit_ratio < bar_close[0]:
 
-                            # We have the same options for selling that we had for purchasing. We can do a market order.
-
-                            # sell_order_id = order.algo_sell(self.symbol, algorithm="market", intent="exit")
-
-                            # Or a limit order, some value around the ask and bid prices.
-
-                            # sell_order_id = order.algo_sell(self.symbol, algorithm="limit", price=md[self.symbol].L1.bid-.05, intent="exit")
-                            # sell_order_id = order.algo_sell(self.symbol, algorithm="limit", price=md[self.symbol].L1.bid*.95, intent="exit")
-
-                            # Finally, a midpoint peg, again, this only works in CloudQuant Elite.
-
-                            sell_order_id = order.algo_sell(self.symbol, algorithm=lime_midpoint_limit_sell,
-                                                            intent="exit")
+                            sell_order_id = order.algo_sell(self.symbol, algorithm="market", intent="exit")
 
                             print('selling out of {0} at {1} due to stock reaching target profit at {2}'.format(
                                 self.symbol, service.time_to_string(service.system_time), bar_close[0]))
@@ -165,7 +122,7 @@ class breakout_purchase(Strategy):
 
         else:
 
-            # close out of our long positions at the end of the day, I always use market for this, because it must fill.
+            # close out of our long positions at the end of the day.
             if self.IsPositionOn == True:
                 sell_order_id = order.algo_sell(self.symbol, "market", intent="exit")
                 self.IsPositionOn = False
